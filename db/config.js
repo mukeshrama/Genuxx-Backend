d467e6efd1afb98e49c67df3f910f6a86352286f
mukeshrama <32187266+mukeshrama@users.noreply.github.com>
2022-12-24 01:24:03 +0530

    database

--- db/config.js
+++ db/config.js
@@ -1,3 +1,17 @@
-const mongoose=require('mongoose');
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/Genux');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/Genux');
+const url = `mongodb+srv://Mukesh:Mukesh@cluster0.4omti3c.mongodb.net/Geexu`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
